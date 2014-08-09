c21bbc5529036a50dbe21d27be23c874f7be7468
pasang sherpa <pasang_sh@hotmail.com>
2014-08-09 00:10:49 -0400

    basic operations added

--- src/doubly-linked-list-adt.js
+++ src/doubly-linked-list-adt.js
@@ -1,6 +1,77 @@
 (function() {
     'use strict';
     var DoublyLinkedList = function() {
+        var root, tail, count = 0;
+        root = tail = null;
+
+        function Node(element) {
+            this.element = element || null;
+            this.next = this.previous = null;
+        }
+
+        function add(element) {
+            var node = new Node(element),
+                current = null;
+            if (root === null) {
+                root = node;
+            } else {
+                current = root;
+                while (current.next !== null) {
+                    current = current.next;
+                }
+                current.next = tail = node;
+            }
+            count++;
+        }
+
+        function remove(index) {
+
+        }
+
+        function first() {
+            if (isEmpty()) {
+                throw new Error('first(): List is empty.');
+            }
+            return root;
+        }
+
+        function get(index) {
+            if (index < 0 || index > count || root === null) {
+                return null;
+            }
+            var midPoint = count / 2,
+                current, i;
+
+            if (index > midPoint) {
+                current = tail;
+                for (i = count; i > index; i--) {
+                    current = current.next;
+                }
+            } else {
+                current = root;
+                for (i = 0; i < index; i++) {
+                    current = current.next;
+                }
+            }
+            return current;
+        }
+
+        function isEmpty() {
+            return count === 0;
+        }
+
+        function size() {
+            return count;
+        }
+
+        return {
+            add: add,
+            remove: remove,
+            first: first,
+            get: get,
+            isEmpty: isEmpty,
+            size: size
+        };
     };
 
     if (typeof define === 'function' && define.amd) {
