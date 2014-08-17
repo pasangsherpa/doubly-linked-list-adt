5137bb885ec0a5a417a6e79b18fec036dabd892c
Pasang Sherpa <pasang_sh@hotmail.com>
2014-08-16 23:52:30 -0700

    indentation fix

--- src/doubly-linked-list-adt.js
+++ src/doubly-linked-list-adt.js
@@ -1,31 +1,31 @@
 (function() {
-    'use strict';
-    var DoublyLinkedList = function() {
-        var root, tail, count = 0;
-        root = tail = null;
+	'use strict';
+	var DoublyLinkedList = function() {
+		var root, tail, count = 0;
+		root = tail = null;
 
-        function Node(element) {
-            this.element = element || null;
-            this.next = this.previous = null;
-        }
+		function Node(element) {
+			this.element = element || null;
+			this.next = this.previous = null;
+		}
 
-        function add(element) {
-            var node = new Node(element),
-                current = null;
-            if (root === null) {
-                root = node;
-            } else {
-                current = root;
-                while (current.next !== null) {
-                    current = current.next;
-                }
-                current.next = tail = node;
-            }
-            count++;
-        }
+		function add(element) {
+			var node = new Node(element),
+				current = null;
+			if (root === null) {
+				root = node;
+			} else {
+				current = root;
+				while (current.next !== null) {
+					current = current.next;
+				}
+				current.next = tail = node;
+			}
+			count++;
+		}
 
-        function remove(index) {
-			var node = get(index? index : count - 1);
+		function remove(index) {
+			var node = get(index ? index : count - 1);
 			if (typeof index !== 'undefined') {
 				root = removeAt(index, root);
 			} else {
@@ -33,72 +33,72 @@
 			}
 			count--;
 			return node;
-        }
+		}
 
-        function removeAt(index, node) {
-        	if (node === null) {
-        		throw new Error('removeAt(): No such element found.');
-        	} else if (index === 0) {
-        		return node.next;
-        	} else {
-        		node.next = removeAt(index - 1, node.next);
-        	}
-        	return node;
-        }
+		function removeAt(index, node) {
+			if (node === null) {
+				throw new Error('removeAt(): No such element found.');
+			} else if (index === 0) {
+				return node.next;
+			} else {
+				node.next = removeAt(index - 1, node.next);
+			}
+			return node;
+		}
 
-        function first() {
-            if (isEmpty()) {
-                throw new Error('first(): List is empty.');
-            }
-            return root;
-        }
+		function first() {
+			if (isEmpty()) {
+				throw new Error('first(): List is empty.');
+			}
+			return root;
+		}
 
-        function get(index) {
-            if (index < 0 || index > count || root === null) {
-                return null;
-            }
-            var midPoint = count / 2,
-                current, i;
+		function get(index) {
+			if (index < 0 || index > count || root === null) {
+				return null;
+			}
+			var midPoint = count / 2,
+				current, i;
 
-            if (index > midPoint) {
-                current = tail;
-                for (i = count; i > index; i--) {
-                    current = current.next;
-                }
-            } else {
-                current = root;
-                for (i = 0; i < index; i++) {
-                    current = current.next;
-                }
-            }
-            return current;
-        }
+			if (index > midPoint) {
+				current = tail;
+				for (i = count; i > index; i--) {
+					current = current.next;
+				}
+			} else {
+				current = root;
+				for (i = 0; i < index; i++) {
+					current = current.next;
+				}
+			}
+			return current;
+		}
 
-        function isEmpty() {
-            return count === 0;
-        }
+		function isEmpty() {
+			return count === 0;
+		}
 
-        function size() {
-            return count;
-        }
+		function size() {
+			return count;
+		}
 
-        return {
-            add: add,
-            remove: remove,
-            first: first,
-            get: get,
-            isEmpty: isEmpty,
-            size: size
-        };
-    };
+		return {
+			add: add,
+			remove: remove,
+			first: first,
+			get: get,
+			isEmpty: isEmpty,
+			size: size
+		};
+	};
 
-    if (typeof define === 'function' && define.amd) {
-        define(function() {
-            return DoublyLinkedList;
-        });
-    } else if (typeof module !== 'undefined' && module.exports) {
-        module.exports = DoublyLinkedList;
-    } else {
-        window.DoublyLinkedList = DoublyLinkedList;
-    }
+	if (typeof define === 'function' && define.amd) {
+		define(function() {
+			return DoublyLinkedList;
+		});
+	} else if (typeof module !== 'undefined' && module.exports) {
+		module.exports = DoublyLinkedList;
+	} else {
+		window.DoublyLinkedList = DoublyLinkedList;
+	}
 })();
